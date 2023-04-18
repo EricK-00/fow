182b1af866fa2ed3824f4168858c39dc124f42dd
EricK-00 <carter000@naver.com>
2023-04-18 16:50:58 +0900

    init

---
+++ fogOfWar/Assets/New/Scripts/PreRenderCalls.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+public class PreRenderCalls : MonoBehaviour
+{
+    public bool UsingURP;
+    public Fog _Fog;
+
+    private float timer = 0f;
+
+    void Awake()
+    {
+        if (UsingURP)
+        {
+            RenderPipelineManager.beginCameraRendering += PreRenderURP;
+        }
+    }
+
+    void OnDisable()
+    {
+        if (UsingURP)
+        {
+            RenderPipelineManager.beginCameraRendering -= PreRenderURP;
+        }
+    }
+
+    void PreRenderURP(ScriptableRenderContext renderContext, Camera obj)
+    {
+        Debug.Log("PreRender");
+        OnPreRender();
+    }
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+    }
+
+    void OnPreRender()
+    {
+        //if (timer < 0.033f)
+        //    return;
+        //else
+        //    timer = 0f;
+
+        Debug.Log("Set");
+        // FOG CALL
+        _Fog.SetCookie();
+    }
+}
